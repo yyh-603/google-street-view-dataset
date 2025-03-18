52fd019842dfd0a64d8a8dbce2e8399411b70535
yyh-603 <yiyanghung603@gmail.com>
2025-03-18 23:19:06 +0800

    add comments

--- build_dataset.py
+++ build_dataset.py
@@ -8,11 +8,21 @@ import time
 
 dotenv.load_dotenv()
 
+'''
+Constants meaning:
+- GOOGLE_MAP_API_KEY: Google Map API key
+- URL: Google Street View API URL
+- START_ID: The starting ID of the picture
+- CITY_MAX: The maximum number of pictures to be collected in each city
+- CITY_LABEL: The list of cities to be collected (Default: 19 cities in Taiwan)
+- CITY_RANGE: The range of latitude and longitude of each city
+'''
+
 GOOGLE_MAP_API_KEY = os.getenv("GOOGLE_MAP_API_KEY")
 URL = "https://maps.googleapis.com/maps/api/streetview"
 
+START_ID = 0
 CITY_MAX = 50
-# CITY_LABEL = ["台北市", "新北市", "桃園市", "台中市", "台南市", "高雄市", "基隆市", "新竹市", "嘉義市", "新竹縣", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義縣", "屏東縣", "宜蘭縣", "花蓮縣", "台東縣"]
 CITY_LABEL = ["Taipei City", "New Taipei City", "Taoyuan City", "Taichung City", "Tainan City", "Kaohsiung City", "Keelung City", "Hsinchu City", "Chiayi City", "Hsinchu County", "Miaoli County", "Changhua County", "Nantou County", "Yunlin County", "Chiayi County", "Pingtung County", "Yilan County", "Hualien County", "Taitung County"]
 CITY_RANGE = {
     "Taipei City": (24.94, 121.44, 25.22, 121.67),
@@ -35,10 +45,19 @@ CITY_RANGE = {
     "Yunlin County": (23.50, 120.11, 23.87, 120.74),
     "Nantou County": (23.43, 120.61, 24.26, 121.36),
 }
-START_ID = 250
 
-def get_picture(lat, lon, heading=0, pitch=0, fov=90):
-    heading = random.randint(0, 359)
+'''
+get_picture: Get a picture from Google Street View API
+- lat: Latitude of the location
+- lon: Longitude of the location
+- heading: The compass heading of the camera(default is random)
+- pitch: The up or down angle of the camera(default is 0)
+- fov: The horizontal field of view of the image(default is 90)
+- return: The content of the picture
+'''
+def get_picture(lat, lon, heading=-1, pitch=0, fov=90):
+    if heading == -1:
+        heading = random.randint(0, 359)
     url = f"{URL}?size=640x640&location={lat},{lon}&heading={heading}&pitch={pitch}&fov={fov}&key={GOOGLE_MAP_API_KEY}"
     response = requests.get(url)
     if response.status_code == 200:
@@ -46,6 +65,12 @@ def get_picture(lat, lon, heading=0, pitch=0, fov=90):
     else:
         return None
     
+'''
+check_picture_exist: Check if the picture exists in the location
+- lat: Latitude of the location
+- lon: Longitude of the location
+- return: True if the picture exists, False if the picture does not exist, None if the request fails
+'''
 def check_picture_exist(lat, lon):
     url = f"{URL}/metadata?location={lat},{lon}&key={GOOGLE_MAP_API_KEY}"
     response = requests.get(url)
@@ -59,51 +84,69 @@ def check_picture_exist(lat, lon):
         print(response.content)
         return None
 
+'''
+get_address: Get the address of the location
+- lat: Latitude of the location
+- lon: Longitude of the location
+- return: a tuple of city and address (city, address)
+'''
 def get_address(lat, lon):
     url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lon}&key={GOOGLE_MAP_API_KEY}"
     response = requests.get(url)
+
     if response.status_code == 200:
         data = json.loads(response.content.decode())
         city = None
         address = None
+
         for item in data['results']:
+            # administrative_area_level_1 is the city in Taiwan
             if 'administrative_area_level_1' in item['types']:
                 city = item['formatted_address']
                 break
+
+            # route is the address
             if 'route' in item['types']:
                 address = item['formatted_address']
+        
         if city == None:
             return None, None
+        
+        # get the city name (original format: "Taipei City, Taiwan")
         city = city.split(',')[0]
+
         return city, address
     else:
         return None, None
     
     
-
-# Example usage
 if __name__ == "__main__":
-    # Read CSV file
 
-    # lat, lon, city, address, picture_name
-    # df = pd.DataFrame(columns=["lat", "lon", "city", "address", "picture_name"])
-    df = pd.read_csv('./output.csv')
+    # dataframe format: lat, lon, city, address, picture_name
+    df = pd.DataFrame(columns=["lat", "lon", "city", "address", "picture_name"])
 
+    # traverse all cities
     for tar_city in CITY_LABEL:
         print("Current city: ", tar_city)
         current_cnt = 0
         try_cnt = 0
         while current_cnt < CITY_MAX:
             time.sleep(0.01)
+
+            # generate random latitude and longitude in the city
             lat = random.uniform(CITY_RANGE[tar_city][0], CITY_RANGE[tar_city][2])
             lon = random.uniform(CITY_RANGE[tar_city][1], CITY_RANGE[tar_city][3])
             try_cnt += 1
-            # print(f"Try {try_cnt}: {lat}, {lon}")
+
             try:
+                # check if the picture exists at the location
                 if not check_picture_exist(lat, lon):
                     continue
+
+                # get the city and address of the location
                 city, address = get_address(lat, lon)
-                # print(city)
+
+                # check if the city is the target city
                 if city != tar_city:
                     continue
 
@@ -117,7 +160,6 @@ if __name__ == "__main__":
                 current_cnt += 1
                 
                 # Append a new row to the DataFrame
-
                 df = pd.concat([df, pd.DataFrame.from_records([{
                     "lat": lat,
                     "lon": lon,
@@ -125,12 +167,10 @@ if __name__ == "__main__":
                     "address": address,
                     "picture_name": picture_name
                 }])], ignore_index=True)
-                # print(df)
+
             except Exception as e:
                 print(e)
                 continue
-            if current_cnt % 5 == 0:
-                print(f"Current {tar_city}: {current_cnt}")
         print(f"Finish {tar_city}, use {try_cnt} times")
 
     # Write DataFrame to a new CSV file
